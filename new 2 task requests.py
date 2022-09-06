e6fd235be6746bf7e92029e229461d1a212c473a
SergeyLebedev26061989 <109577543+SergeyLebedev26061989@users.noreply.github.com>
2022-09-06 14:10:47 +0300

    Add files via upload

--- new 2 task requests.py
+++ new 2 task requests.py
@@ -1,20 +1,35 @@
-import yadisk
+from pprint import pprint
 
-class YaUploader:
-    def __init__(self, token: str):
+import requests
+
+class YandexDisk:
+
+    def __init__(self, token):
         self.token = token
 
-    def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
-        y = yadisk.YaDisk(token=token)
-        y.upload("text.txt", "/text.txt")
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
 
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
 
+    def upload_file_to_disk(self, disk_file_path, filename):
+        href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+#
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = input('путь к файлу: ')
-    token = input('введите токкен: ')
-    uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+    path_to_file = input('введите путь к загружаемому файлу: ')
+    token = input('введите свой TOKEN: ')
+    uploader = YandexDisk(token)
+    result = uploader.upload_file_to_disk('NetologyTEST/xls-file.xlsx', 'переделки июль.xlsx')
